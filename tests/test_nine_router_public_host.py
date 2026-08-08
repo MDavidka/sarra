117893fa9b61559182ac23508271719d7538365f
Márton I. Dávid <m.david2023@koskaroly.ro>
2026-08-08 23:30:19 +0300

    test(9router): lock managed public GUI hostname

---
+++ tests/test_nine_router_public_host.py
@@ -0,0 +1,4 @@
+def test_managed_9router_public_host_is_dedicated() -> None:
+    from syte.caddy_routes import NINE_ROUTER_PUBLIC_HOST
+
+    assert NINE_ROUTER_PUBLIC_HOST == "9router.sycord.site"
